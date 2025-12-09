2f58ab4f09e6d5091069f0d08543588a1f64583a
GitStudent0-0 <marinakadykova05@gmail.com>
2025-12-09 09:26:04 +0300

    написан класс person

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,64 @@
 package org.example;
 
-public class Person {
+public class Person
+{
+    private int id;
+    private String name;
+    private String gender;
+    private Division division;
+    private double salary;
+    private String birthDate;
+
+    public Person(int id, String name, String gender, Division division, double salary, String birthDate)
+    {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.division = division;
+        this.salary = salary;
+        this.birthDate = birthDate;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public Division getDivision()
+    {
+        return division;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public String getBirthDate()
+    {
+        return birthDate;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", division=" + division +
+                ", salary=" + salary +
+                ", birthDate='" + birthDate + '\'' +
+                '}';
+    }
 }
