640389382a91dfbe4eb34e1eb20120931e95c4e1
GitStudent0-0 <marinakadykova05@gmail.com>
2025-12-09 18:40:22 +0300

    написан main для проверки работы программы

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class Main {
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        CsvParser parser = new CsvParser();
+        List<Person> people = parser.parse("foreign_names.csv");
+
+        System.out.println("Всего загружено людей: " + people.size());
+        System.out.println("\nПервые 10 записей:");
+
+        for (int i = 0; i < Math.min(10, people.size()); i++)
+            System.out.println(people.get(i));
+
+    }
 }
